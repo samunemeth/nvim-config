d4d502242a7fef86ad1cc770d0a3686d5071d14b
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-16 23:47:57 +0200

    Updated key maps

---
+++ after/plugin/fugitive.lua
@@ -0,0 +1,4 @@
+--- Configuration for vim-fugitive plugin ---
+
+vim.opt.splitbelow = true
+vim.keymap.set("n", "<leader>g", vim.cmd.Git)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,4 +5,4 @@ local builtin = require("telescope.builtin")
 -- Keybindings
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-vim.keymap.set("n", "<leader>gg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
