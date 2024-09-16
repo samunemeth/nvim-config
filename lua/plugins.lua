46ab960c12885e3bcec974ab286ec474dc69653e
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-16 20:13:16 +0200

    Moved plugins to seprate file

---
+++ lua/plugins.lua
@@ -0,0 +1,23 @@
+--- Plugins ---
+
+local Plug = vim.fn['plug#']
+
+--- Need to load before plugins ---
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+--- Plugins ---
+
+-- All packages are listed here
+-- Any package without a comment after it is a requirement of the next package.
+
+vim.call('plug#begin')
+
+Plug('lervag/vimtex') -- For compiling latex
+Plug('SirVer/ultisnips') -- For creating snippets
+Plug('nvim-tree/nvim-tree.lua') -- For file tree
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' }) -- For syntax highlighting
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' }) -- For fuzzy finding
+
+vim.call('plug#end')
