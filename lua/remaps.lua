49d7c1c466000e349ab20c5adba77bf0bbd6b54d
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-16 22:01:28 +0200

    Added word replace command to remaps

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<leader>th', '<C-w>s<C-w>w<cmd>ter<cr>')
 -- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Replace word under cursor
+vim.keymap.set("n", "<leader>h", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
