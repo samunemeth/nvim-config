61b9cc64cd9f0e6c833cdefc6dfcb920a00a2da7
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-16 21:49:28 +0200

    Added custom keybindings to nvimtree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,6 +1,24 @@
 --- Nvimtree plugin settings ---
 
-require("nvim-tree").setup()
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- Default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- Custom mappings
+  vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', 't', '<C-w>w', opts('Change window'))
+end
+
+require("nvim-tree").setup({
+  on_attach = my_on_attach,
+})
+
 vim.opt.termguicolors = true
 
 -- Keybinding
