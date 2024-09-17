99455406a9ba9bdd1fdc5e48eb1eac26c8bdb934
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-17 12:03:26 +0200

    Removed InitLuaEdit and InitLuaReload commands

--- lua/commands.lua
+++ lua/commands.lua
@@ -4,7 +4,3 @@
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Q", "q", {})
 
--- Opens and reloads vim configuration files
-vim.api.nvim_create_user_command("InitLuaEdit", vim.fn.expand("e $MYVIMRC"), {})
-vim.api.nvim_create_user_command("InitLuaReload", vim.fn.expand("so $MYVIMRC"), {})
-
