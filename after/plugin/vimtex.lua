33358a2a808f9936d1ad28c6c7061eba9a68163b
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-17 12:02:42 +0200

    Disabled vimtex mappings

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,12 +1,13 @@
 --- Vimtex configuration ---
 ---
+vim.opt.conceallevel = 1
+
 vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_method = "zathura"
-vim.opt.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_indent_lists = {}
+vim.g.vimtex_mappings_enabled = false -- Disable keybindings to not flood the custom keybindings
 vim.g.vimtex_quickfix_ignore_filters = {
 	"Underfull",
 	"Overfull",
 }
-
