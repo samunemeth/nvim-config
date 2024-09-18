14dc05e919389a171e537d2513517369e79d28c3
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-18 20:44:33 +0200

    Removed UltiSnips after script, as it doesn't affect the plugin

--- after/plugin/ultisnips.lua
+++
@@ -1,9 +0,0 @@
---- UltiSnips plugin configuration ---
-
-vim.g.UltiSnipsExpandTrigger = "<Tab>"
-vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
-vim.g.UltiSnipsSnippetDirectories = {
-	vim.fn.expand("$HOME/.config/nvim/UltiSnips"),
-}
-
