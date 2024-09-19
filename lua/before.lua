51ccb953658df4abfb13470aa35ff323d8c5f919
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-19 09:40:11 +0200

    Added description to before file

--- lua/before.lua
+++ lua/before.lua
@@ -1,6 +1,8 @@
 --- For script that need to load before plugins ---
 
 -- UltiSnips
+-- This configuration needs to load before the plugin does.
+-- In the future, I may need to switch to LuaSnips
 vim.g.UltiSnipsExpandOrJumpTrigger = "<Tab>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
 vim.g.UltiSnipsSnippetDirectories = {
