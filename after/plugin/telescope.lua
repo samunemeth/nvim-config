9a20376efe4f2bf50fcbc03a246684e8f21c906b
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-09-21 09:30:37 +0200

    Added hidden option

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,17 @@
 --- Telescope plugin settings ---
 
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
 
+telescope.setup {
+	pickers = {
+		find_files = {
+			hidden = true
+		}
+	}
+} 
+
 -- Keybindings
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
