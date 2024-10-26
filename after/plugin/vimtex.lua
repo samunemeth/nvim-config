804ab5c11a6d51422b53fa0e0e610c71a692ba5e
Samu Nemeth <nemeth.samu.0202@gmail.com>
2024-10-26 23:09:32 +0200

    Vimtex conceal all possible

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -9,3 +9,19 @@ vim.g.vimtex_indent_lists = {}
 vim.g.vimtex_mappings_enabled = false
 vim.g.vimtex_quickfix_ignore_filters = { "Underfull", "Overfull" }
 vim.cmd.highlight({ "Conceal", "guifg=white" })
+
+vim.g.vimtex_syntax_conceal = {
+	["accents"] = 1,
+	["ligatures"] = 1,
+	["cites"] = 1,
+	["fancy"] = 1,
+	["spacing"] = 1,
+	["greek"] = 1,
+	["math_bounds"] = 1,
+	["math_delimiters"] = 1,
+	["math_fracs"] = 1,
+	["math_super_sub"] = 1,
+	["math_symbols"] = 1,
+	["sections"] = 1,
+	["styles"] = 1,
+}
