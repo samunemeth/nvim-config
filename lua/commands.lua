b291f353c9f73f83402124cf6d59e881e186f5e1
Samu Nemeth <nemeth.samu.0202@gmail.com>
2025-01-22 22:55:08 +0100

    Started implementing markdown compiler.

--- lua/commands.lua
+++ lua/commands.lua
@@ -10,3 +10,27 @@ vim.api.nvim_create_user_command('Systemd',
   end,
   { nargs = 1 }
 )
+
+vim.api.nvim_create_user_command('Compile',
+  function(opts)
+    print("Compiling to PDF...")
+    local function compile_finished(chan_id, data, name)
+      print("Compiled to PDF.")
+    end
+    vim.fn.jobstart("pandoc -s -V papersize:a4 -o " .. vim.fn.expand("%:p:r") .. ".pdf ".. vim.fn.expand("%:p"), { on_exit = compile_finished })
+  end,
+  {}
+)
+
+vim.api.nvim_create_user_command('Zathura',
+  function(opts)
+    local to_open = vim.fn.expand("%:p:r") .. ".pdf"
+    if vim.fn.filereadable(to_open) then
+      print("Opening zathura...")
+      vim.fn.jobstart("zathura " .. to_open)
+    else
+      print("PDF not found!")
+    end
+  end,
+  {}
+)
