5bd5545b42b2f15943bb02c36cac9b7bed2e511f
Samu Nemeth <nemeth.samu.0202@gmail.com>
2025-02-24 11:36:26 +0100

    Grouped plugins by category

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,25 +8,34 @@ vim.g.loaded_netrwPlugin = 1
 
 --- Plugins ---
 
--- All packages are listed here
+-- All packages used are imported here.
 -- Any package without a comment after it is a requirement of the next package.
+-- Packages are grouped based on functionality.
 
 vim.call("plug#begin")
 
+-- LaTeX related
 Plug("lervag/vimtex") -- For compiling latex
+Plug("frabjous/knap") -- Live latex compile
+Plug("jbyuki/nabla.nvim") -- For equation preview in the editor.
 Plug("SirVer/ultisnips") -- For creating snippets
+
+-- File Management
 Plug("nvim-tree/nvim-tree.lua") -- For file tree
-Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" }) -- For syntax highlighting
 Plug("nvim-lua/plenary.nvim")
 Plug("nvim-telescope/telescope.nvim", { ["tag"] = "0.1.8" }) -- For fuzzy finding
+
+-- Visuals
 Plug("itchyny/lightline.vim") -- For the fancy status bar
+Plug("karb94/neoscroll.nvim") -- For smooth scrolling
+
+-- Change Tracking
 Plug("mbbill/undotree") -- For the undo tree
 Plug("tpope/vim-fugitive") -- For git
-Plug("karb94/neoscroll.nvim") -- For smooth scrolling
--- Plug("toppair/peek.nvim", { ["do"] = "deno task --quiet build:fast" }) -- For markdown preview.
-Plug("jbyuki/nabla.nvim") -- For equation preview in the editor.
+
+-- LSP and Syntax
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" }) -- For syntax highlighting
 Plug("williamboman/mason.nvim") -- For installing and managing LSP servers.
 Plug("neovim/nvim-lspconfig") -- Pre-made configurations for LSP servers.
-Plug("frabjous/knap") -- Live latex compile
 
 vim.call("plug#end")
