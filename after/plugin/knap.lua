d69978ae8a7de3e96debd244eaa559a8c8dc0121
Samu Nemeth <nemeth.samu.0202@gmail.com>
2025-02-26 09:21:47 +0100

    Fully switched to knap for latex compilation

--- after/plugin/knap.lua
+++ after/plugin/knap.lua
@@ -3,22 +3,33 @@ local knap = require("knap")
 
 -- Knap settings
 local gknapsettings = {
-    textopdfviewerlaunch = "zathura --synctex-editor-command 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%{input}'\"'\"',%{line},0)\"' %outputfile%",
+    textopdfviewerlaunch = "zathura " ..
+        "--synctex-editor-command 'nvim --headless -es --cmd " ..
+        "\"lua require('\"'\"'knaphelper'\"'\"').relayjump(" ..
+        "'\"'\"'%servername%'\"'\"','\"'\"'%{input}'\"'\"',%{line},0)\"' " ..
+        "%outputfile%",
     textopdfviewerrefresh = "none",
-    textopdfforwardjump = "zathura --synctex-forward=%line%:%column%:%srcfile% %outputfile%",
-    textopdf = "pdflatex -jobname \"$(basename -s .pdf %outputfile%)\" -halt-on-error",
+    textopdfforwardjump = "zathura " .. 
+        "--synctex-forward=%line%:%column%:%srcfile% %outputfile%",
+    textopdf = "pdflatex " ..
+        "-jobname \"$(basename -s .pdf %outputfile%)\" -halt-on-error",
     textopdfbufferasstdin = true,
 }
 vim.g.knap_settings = gknapsettings
 
 -- Start compilation when a LaTeX file is opened
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile"}, {
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufWrite" }, {
 	pattern = { "main.tex" },
-    callback = function() knap.toggle_autopreviewing() end,
+    callback = function() knap.process_once() end,
 })
 
 -- Remaps to useful commands
-vim.keymap.set({ 'n', 'v', 'i' }, '<F5>', function() knap.process_once() end)
-vim.keymap.set({ 'n', 'v', 'i' }, '<F6>', function() knap.close_viewer() end)
-vim.keymap.set({ 'n', 'v', 'i' }, '<F7>', function() knap.toggle_autopreviewing() end)
-vim.keymap.set({ 'n', 'v', 'i' }, '<F8>', function() knap.forward_jump() end)
+vim.keymap.set({"n", "v"}, "<leader>x", function()
+    knap.close_viewer()
+end)
+vim.keymap.set({"n", "v"}, "<leader>c", function()
+    knap.toggle_autopreviewing()
+end)
+vim.keymap.set({"n", "v"}, "<leader>y", function()
+    knap.forward_jump()
+end)
