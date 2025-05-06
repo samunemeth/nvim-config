14913724646dc227246df0619bfb143d8e6c29cc
samunemeth <nemeth.samu.0202@gmail.com>
2025-05-06 16:22:52 +0200

    Implemented wsl copy and paste functionality

--- lua/settings.lua
+++ lua/settings.lua
@@ -32,6 +32,14 @@ vim.opt.spellcapcheck = ""
 
 -- Clipboard configuration
 vim.opt.clipboard = "unnamedplus"
+if vim.fn.has('wsl') == 1 then
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+  })
+end
 
 -- Panel behaviour
 vim.opt.splitbelow = true
