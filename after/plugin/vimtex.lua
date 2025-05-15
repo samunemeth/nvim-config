35e007d449d1734a7f2b6c51a3baf4b6a71ccd3a
Samu Nemeth <nemeth.samu.0202@gmail.com>
2025-05-15 23:38:19 +0200

    Enabled Vimtex mappings.

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -6,7 +6,7 @@ vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_method = "zathura"
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_indent_lists = {}
-vim.g.vimtex_mappings_enabled = false
+vim.g.vimtex_mappings_enabled = true
 vim.g.vimtex_quickfix_ignore_filters = { "Underfull", "Overfull" }
 vim.cmd.highlight({ "Conceal", "guifg=white" })
 
